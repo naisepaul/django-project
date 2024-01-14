37a8febaaa44094b59d993981eb1695586f6d107
naisepaul <>
2024-01-14 22:14:14 +0000

    changed DEBUG to True

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ot#^s95*b487mvm8iopdft+!*pohc+^#2x_dukjhjy%%bh-8l6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-naisepaul-django-project-kavi9ex3fr.us2.codeanyapp.com','.herokuapp.com']
 
